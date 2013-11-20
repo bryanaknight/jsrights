a10c0c9a0124ce3093f97b104b0ecd1e273bde28
Bryana Knight <brknig11@gmail.com>
2013-11-20 16:16:20 -0700

    first commit

---
+++ application.js
@@ -0,0 +1,33 @@
+console.log("I loaded application.js!");
+
+$(function () {
+  var backToTop = "<a href='#top'>Back to Top</a>";
+  $("div.article").append(backToTop);
+
+  var tableOfContents = "<h2 id='toc'><a name='toc'>Table of Contents</a></h2>"
+  var unorderedList = "<ul name='toc'></ul>"
+  $("h1:first").after(tableOfContents)
+  $("h1:first").after(unorderedList)
+
+
+  $("div.article h2").css('background', '#CCF')
+  $("div.article h2").each(function (){
+    var title = $(this).text();
+    var url = title.trim().toLowerCase().replace(" ","_");
+    console.log(url);
+    var targetAnchor = "<a name='" + url + "'/>";
+    $(this).before(targetAnchor)
+    console.log(title);
+    var listItem = "<li>" + "<a href='#" + url + "'>" + title + "</a>"+  "</li>";
+    $('#toc').append(listItem);
+    var toggleLink = $("<a href='#'>(hide)</a>");
+    toggleLink.on('click', function(event){
+      $(this).siblings().toggle();
+    });
+    $(this).after(toggleLink);
+    var oldText = $(this).text();
+    var newText = (oldText === '(hide)') ? '(show)' : '(hide)'
+    
+  });
+
+});
